9ea6161b5cc0177e8820a219d4eedbde0907a5c3
Bhawesh Madhwani <bhawesh.madhwani@gmail.com>
2021-03-10 23:21:04 +0530

    1st

---
+++ app/src/main/java/com/sherlocked/scicalculator/MainActivity.java
@@ -0,0 +1,136 @@
+package com.sherlocked.scicalculator;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.text.SpannableStringBuilder;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.view.View;
+import org.mariuszgromada.math.mxparser.*;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView previousCalculation;
+    private EditText display;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        previousCalculation = findViewById(R.id.previousCalculationView);
+        display = findViewById(R.id.displayEditText);
+
+        display.setShowSoftInputOnFocus(false);
+
+    }
+
+    private void updateText(String strToAdd){
+        String oldString = display.getText().toString();
+
+        int cursorPos = display.getSelectionStart();
+
+        String leftStr = oldString.substring(0,cursorPos);
+        String rightStr = oldString.substring(cursorPos);
+
+        display.setText(String.format("%s%s%s",leftStr,strToAdd,rightStr));
+
+        display.setSelection(cursorPos + strToAdd.length());
+    }
+
+    public void zeroBTNPush(View view) {
+        updateText(getResources().getString(R.string.zeroText));
+    }
+
+    public void oneBTNPush(View view) {
+        updateText(getResources().getString(R.string.oneText));
+    }
+
+    public void twoBTNPush(View view) {
+        updateText(getResources().getString(R.string.twoText));
+    }
+
+    public void threeBTNPush(View view) {
+        updateText(getResources().getString(R.string.threeText));
+    }
+
+    public void fourBTNPush(View view) {
+        updateText(getResources().getString(R.string.fourText));
+    }
+
+    public void fiveBTNPush(View view) {
+        updateText(getResources().getString(R.string.fiveText));
+    }
+
+    public void sixBTNPush(View view) {
+        updateText(getResources().getString(R.string.sixText));
+    }
+
+    public void sevenBTNPush(View view) {
+        updateText(getResources().getString(R.string.sevenText));
+    }
+
+    public void eightBTNPush(View view) {
+        updateText(getResources().getString(R.string.eightText));
+    }
+
+    public void nineBTNPush(View view) {
+        updateText(getResources().getString(R.string.nineText));
+    }
+
+    public void addBTNPush(View view) {
+        updateText(getResources().getString(R.string.addText));
+    }
+
+    public void subtractBTNPush(View view) {
+        updateText(getResources().getString(R.string.subtractText));
+    }
+
+    public void multiplyBTNPush(View view) {
+        updateText(getResources().getString(R.string.multiplyText));
+    }
+
+    public void divideBTNPush(View view) {
+        updateText(getResources().getString(R.string.divideText));
+    }
+
+    public void clearBTNPush(View view) {
+        display.setText("");
+    }
+
+    public void parOpenBTNPush(View view) {
+        updateText(getResources().getString(R.string.parenthesesOpenText));
+    }
+
+    public void parCloseBTNPush(View view) {
+        updateText(getResources().getString(R.string.parenthesesCloseText));
+    }
+
+    public void decimalBTNPush(View view) {
+        updateText(getResources().getString(R.string.decimalText));
+    }
+
+    public void equalBTNPush(View view) {
+        String userExp = display.getText().toString();
+
+        userExp = userExp.replaceAll(getResources().getString(R.string.divideText),"/");
+        userExp = userExp.replaceAll(getResources().getString(R.string.multiplyText),"*");
+
+        Expression exp = new Expression(userExp);
+        String result = String.valueOf(exp.calculate());
+        display.setText(result);
+        display.setSelection(result.length());
+    }
+    public void backspaceBTNPush(View view) {
+        int cursorPos = display.getSelectionStart();
+        int textLen = display.getText().length();
+        if(cursorPos!=0 && textLen!=0) {
+            SpannableStringBuilder selection = (SpannableStringBuilder) display.getText();
+            selection.replace(cursorPos-1,cursorPos,"");
+            display.setText(selection);
+            display.setSelection(cursorPos-1);
+        }
+    }
+
+}
